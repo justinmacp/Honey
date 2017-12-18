5f409218da37d15feff3cf629642f5de80189dfb
justinmacp <justin.macpherson@ryerson.ca>
2017-12-18 18:29:28 +0100

    Add text manipulation defs

---
+++ main.py
@@ -0,0 +1,8 @@
+import utils
+
+sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('data.txt')
+wordcount5 = retrieve_wordcount(sentences5)
+wordcount4 = retrieve_wordcount(sentences4)
+wordcount3 = retrieve_wordcount(sentences3)
+wordcount2 = retrieve_wordcount(sentences2)
+wordcount1 = retrieve_wordcount(sentences1)
