29b5adc1c3d4aae165869ad7616c7c86808da5c8
justinmacp <justin.macpherson@ryerson.ca>
2017-12-20 09:42:13 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import utils
+from utils import *
 
 sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('data.txt')
 wordcount5 = retrieve_wordcount(sentences5)
@@ -6,3 +6,4 @@ wordcount4 = retrieve_wordcount(sentences4)
 wordcount3 = retrieve_wordcount(sentences3)
 wordcount2 = retrieve_wordcount(sentences2)
 wordcount1 = retrieve_wordcount(sentences1)
+dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount1,wordcount2,wordcount3,wordcount4,wordcount5)
