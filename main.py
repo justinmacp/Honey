219100ea585214808eca2a84dd2844b5015c8181
justinmacp <justin.macpherson@ryerson.ca>
2017-12-21 12:42:13 +0100

    updated to run on codewords.json

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from utils import *
 
-sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('data.txt')
+sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('codewords.json')
 wordcount5 = retrieve_wordcount(sentences5)
 wordcount4 = retrieve_wordcount(sentences4)
 wordcount3 = retrieve_wordcount(sentences3)
 wordcount2 = retrieve_wordcount(sentences2)
 wordcount1 = retrieve_wordcount(sentences1)
 dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount1,wordcount2,wordcount3,wordcount4,wordcount5)
+qu
