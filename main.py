29b2c081fa3103fd5fc6b3709f50203a81877552
justinmacp <justin.macpherson@ryerson.ca>
2017-12-26 13:22:02 +0100

    added code to test on data.txt

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+'''
+    training on codewords.json; results placed in dict.json
+'''
 from utils import *
 
 sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('codewords.json')
@@ -6,5 +9,8 @@ wordcount4 = retrieve_wordcount(sentences4)
 wordcount3 = retrieve_wordcount(sentences3)
 wordcount2 = retrieve_wordcount(sentences2)
 wordcount1 = retrieve_wordcount(sentences1)
-dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount1,wordcount2,wordcount3,wordcount4,wordcount5)
-qu
+dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount1,wordcount2,wordcount3,wordcount4,wordcount5,'dict.json')
+'''
+    testing on data.txt using dict.json; results placed in dataout.json
+'''
+sentence_filter('data.txt','dataout.json','dict.json')
