72bdf4757a55d50d286af31c3a63ec296f4517fd
justinmacp <justin.macpherson@ryerson.ca>
2017-12-29 15:51:28 +0100

    supplemented with considerations for GloVe implementation: added a total vocab count for statistical analysis later on

--- Preprocessing/main.py
+++ Preprocessing/main.py
@@ -3,12 +3,18 @@
 '''
 from utils import *
 
+vocab_count = list()    #for GloVe purposes
 sentences1,sentences2,sentences3,sentences4,sentences5, = retrieve_sentences('Training_Data/codewords.json')
 wordcount5 = retrieve_wordcount(sentences5)
 wordcount4 = retrieve_wordcount(sentences4)
 wordcount3 = retrieve_wordcount(sentences3)
 wordcount2 = retrieve_wordcount(sentences2)
 wordcount1 = retrieve_wordcount(sentences1)
+vocab_count.append(wordcount1)
+vocab_count.append(wordcount2)
+vocab_count.append(wordcount3)
+vocab_count.append(wordcount4)
+vocab_count.append(wordcount5)
 dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount1,wordcount2,wordcount3,wordcount4,wordcount5,'Training_Results/dict.json')
 '''
     testing on data.txt using dict.json; results placed in dataout.json
