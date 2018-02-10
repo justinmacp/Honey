1dce668970de29d967cff0a28d81dcfaa444aa89
justinmacp <justin.macpherson@ryerson.ca>
2018-02-10 18:36:03 -0500

    added first stage of post-processing

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from utils import *
+from glove_model import *
+
 '''
     training on codewords.json; results placed in dict.json
 '''
-from utils import *
+sentence_list = retrieve_sentences('Preprocessing/Training_Data/codewords2.json')
+word_count = retrieve_wordcount(sentence_list)
+word_bias(word_count,'Preprocessing/Training_Results/dict3.json')
 
-sentences50,sentences60,sentences70,sentences80,sentences90 = retrieve_sentences('Preprocessing/Training_Data/codewords2.json')
-wordcount90 = retrieve_wordcount(sentences90)
-wordcount80 = retrieve_wordcount(sentences80)
-wordcount70 = retrieve_wordcount(sentences70)
-wordcount60 = retrieve_wordcount(sentences60)
-wordcount50 = retrieve_wordcount(sentences50)
-dependent_words, expected_value, standard_deviation, probability_given_5, probability_given_4, probability_given_3, probability_given_2, probability_given_1 = wordbias(wordcount50,wordcount60,wordcount70,wordcount80,wordcount90,'Preprocessing/Training_Results/dict.json')
 '''
     testing on data.txt using dict.json; results placed in dataout.json
 '''
-sentence_filter('Preprocessing/Test_Data/data.txt','Preprocessing/Results/dataout.json','Preprocessing/Training_Results/dict.json')
+sentence_filter('Preprocessing/Test_Data/data.txt','Preprocessing/Results/dataout.json','Preprocessing/Training_Results/dict3.json')
+
+'''
+    Post processing: employing the GloVe algorithm
+'''
+fill_co_occurrence_matrix('Preprocessing/Results/dataout.json','Postprocessing/cooccur.json')
